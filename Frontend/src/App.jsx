5156aada37397c6670219e01230f0858c8f295a6
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-12-22 10:46:52 +0530

    added home page routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import 'react';
-import { BrowserRouter as Router,} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes,} from 'react-router-dom';
 import Header from './components/Header';
-
+import Home from './components/Home';
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </Router>
   );
 };
